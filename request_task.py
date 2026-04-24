e6c5c066f08a46347c0bd828798884b2bba71c29
hikkarikka <karbozovadana01@gmail.com>
2026-04-24 15:40:12 +0500

    requests

--- request_task.py
+++ request_task.py
@@ -8,5 +8,57 @@ def menu():
     print("4.выйти")
 
 def get_post(post_id):
-    response = requests.get(f"{url}/{post_id}")
-    print(response.json())
+    try:
+        response = requests.get(f"{url}/{post_id}")
+        if response.status_code == 200:
+            print(response.json())
+        else:
+            print("пост не найден")
+    except Exception as e:
+        print(f"ошибка {e}")
+
+
+def all_posts():
+    try:
+        response = requests.get(url)
+        posts = response.json()
+        for i in posts [:10]:
+            print(i["title"])
+    except Exception as e:
+        print(f"ошибка {e}")
+
+def create_post():
+    try:
+        title = input("введите title: ")
+        body = input("введите Body: ")
+        data = {
+        "title": title,
+        "body": body,
+        "userId": 1
+        }
+        response = requests.post(url, json=data)
+        print(f"создан {response.json}")
+    except Exception as e:
+        print(f"ошибка {e}")
+
+def main():
+    while True:
+        menu()
+        try:
+            num = int(input("Введите цифру команды: "))
+            if num == 1:
+                post_id = input("введите id: ")
+                get_post(post_id)
+            elif num == 2:
+                all_posts()
+            elif num == 3:
+                create_post()
+            elif num == 4:
+                break
+            else:
+                print("неверный выбор")
+        except ValueError:
+            print("Введите число а не букву")
+
+if __name__ == "__main__":
+    main()   
---
+++ request_task_class.py
@@ -0,0 +1,41 @@
+import requests
+
+
+class APIClient:
+    def __init__(self, url):
+        self.url = url
+
+# def get(self, endpoint):
+#     try:
+#         response = requests.get(f"{self.url}/{endpoint}")
+#         if response.status_code == 200:
+#             print(response.json())
+#         else:
+#             print("пост не найден")
+#     except Exception as e:
+#         print(f"ошибка {e}")
+
+    def get(self, url):
+        try:
+            response = requests.get(f"{self.url}/{endpoint}")
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f'ошибка GET: {e}')
+            return None
+            
+
+
+
+class PostService:
+    pass
+class App:
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
